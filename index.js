88504d659f5a44fe53f49bee8e34aa4b4a0ef35a
Dmitry Tsvettsikh <dmitrycvet@gmail.com>
2017-07-12 03:13:48 +0500

    improve package

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 'use strict'
 
 module.exports = function(n, min, max) {
+  if (arguments.length < 2) {
+    throw new SyntaxError('Expected number of arguments should be at least 2')
+  }
 
-  if (arguments.length <= 2) {
-
-    if (arguments.length === 1) {
-      throw new SyntaxError('Expected number of arguments should be at least 2')
-    } else {
-      max = min
-      min = 0
-    }
+  if (arguments.length === 2) {
+    max = min
+    min = 0
   }
 
   if (typeof n !== 'number'
@@ -17,10 +15,6 @@ module.exports = function(n, min, max) {
     || typeof max !== 'number') {
 
     throw new TypeError('Expected all arguments to be numbers')
-  }
-
-  if (min > max) {
-    throw new RangeError('Expected minimum range to be <= max')
   }
 
   return n >= Math.min(min, max) && n <= Math.max(max, min)
